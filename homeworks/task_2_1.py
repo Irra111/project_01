49edf8cf60cd974b0d29b3d427ea26d4e4d9f931
Irra111 <pakhtacore@bk.ru>
2023-05-29 21:23:05 +0300

    fix file task_2_1.py

--- homeworks/task_2_1.py
+++ homeworks/task_2_1.py
@@ -14,3 +14,12 @@ def maximum(arr):
             max_number = num
 
     return max_number
+
+assert maximum([4,6,2,1,9,63,-134,566]) == 566
+assert minimum([4,6,2,1,9,63,-134,566]) == -134
+assert maximum([-52, 56, 30, 29, -54, 0, -110]) == 56
+assert minimum([-52, 56, 30, 29, -54, 0, -110]) == -110
+assert maximum([42, 54, 65, 87, 0]) == 87
+assert minimum([42, 54, 65, 87, 0]) == 0
+assert maximum([5]) == 5
+assert minimum([5]) == 5
