4c571d9bc64ec87b8f0474455362682fb5da8c8d
Irra111 <pakhtacore@bk.ru>
2023-05-29 17:23:05 +0300

    Add file task_1_1.py

---
+++ homeworks/task_1_1.py
@@ -0,0 +1,3 @@
+my_favorite_songs = 'Waste a Moment, Staying\' Alive, A Sorta Fairytale, Start Me Up, New Salvation'
+songs = my_favorite_songs.split(',')
+print(songs[0], songs[-1], songs[1], songs[-2])
